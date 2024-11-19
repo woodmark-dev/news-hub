3c9a33cab5c8e0e17b7a43ef1448764b0b8371b7
mutiat <mutiat900@gmail.com>
2024-11-19 18:28:23 +0100

    created filter page

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,11 +1,15 @@
 import NavBar from "../components/nav-bar/nav-bar";
 import TrendingNews from "./home-sections/trending-news/trending-news";
+import Filterpage from "../components/filterpage/filter";
+
 
 export default function LandingPage() {
 	return (
 		<div className="LandingPage">
 			<NavBar />
 			<TrendingNews />
+			
+			
 		</div>
 	);
 }
